71b7fa59d6d17b000830734bec7e0f6fc0acd3db
Part1x <part1xtube@gmail.com>
2024-07-06 10:47:28 +0300

    Add files via upload

---
+++ saev1.py
@@ -0,0 +1,9 @@
+#1st program
+number1 = 9**0.5
+print(number1*5)
+#2nd program
+print(9.99>9.98 and 1000 != 100.1)
+#3rd program
+print(1234%1000//10 + 5678%1000//10)
+#4rd program 13.42 42.13
+print (int(13.42) == int((42.13 - int(42.13)) * 100) or (int(42.13) == int((13.42 - int(13.42)) * 100)))
